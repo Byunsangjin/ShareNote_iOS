92bf8a965877e7195c72b9143e60eed3412a8541
Byunsangjin <qustkdwls1@naver.com>
2020-12-03 23:28:29 +0900

    Init Project

---
+++ ShareNote/ShareNote/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShareNote
+//
+//  Created by sjbyun on 2020/12/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
