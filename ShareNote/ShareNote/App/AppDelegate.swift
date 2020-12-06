ecdd29f41e1289ff05090391589c42fde4244287
Byunsangjin <qustkdwls1@naver.com>
2020-12-06 20:09:22 +0900

    AppKey Update

--- ShareNote/ShareNote/App/AppDelegate.swift
+++ ShareNote/ShareNote/App/AppDelegate.swift
@@ -11,11 +11,8 @@ import KakaoSDKAuth
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        RxKakaoSDKCommon.initSDK(appKey: "NATIVE_APP_KEY")
+        RxKakaoSDKCommon.initSDK(appKey: KAKAO_NATIVE_KEY)
         
         return true
     }
@@ -43,7 +40,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
 
