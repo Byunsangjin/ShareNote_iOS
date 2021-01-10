574a2169bbbd99cf36f9cfc2edf35cd08b8d1cf4
Byunsangjin <qustkdwls1@naver.com>
2021-01-10 15:51:23 +0900

    [SimplePassword] SimplePasswordViewController 추가

---
+++ ShareNote/ShareNote/ViewControllers/Login/SimplePasswordViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SimplePasswordViewController.swift
+//  ShareNote
+//
+//  Created by sjbyun on 2021/01/10.
+//
+
+import UIKit
+
+class SimplePasswordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
