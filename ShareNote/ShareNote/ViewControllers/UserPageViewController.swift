2377a73d346f46237ee1e35b52ffd9ba58434651
Byunsangjin <qustkdwls1@naver.com>
2021-01-21 22:43:25 +0900

    UserPageViewController 임시 막음
    - 사용하지 않아 임시 막음 처리

--- ShareNote/ShareNote/ViewControllers/UserPageViewController.swift
+++ ShareNote/ShareNote/ViewControllers/UserPageViewController.swift
@@ -12,52 +12,52 @@ import RxSwift
 import UIKit
 
 class UserPageViewController: UIViewController {
-    // MARK: Actions
-    @IBOutlet var titleLabel: UILabel!
-    @IBOutlet var logoutButton: UIButton!
-    
-    // MARK: Variables
-    var disposeBag = DisposeBag()
-    
-    // MARK: Methods
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        let loginType = UserDefaults.standard.value(forKey: "loginType") as! String
-        if loginType.elementsEqual("Kakao") {
-            kakaoLogoutBtnInit()
-        } else if loginType.elementsEqual("Naver") {
-            naverLogoutBtnInit()
-        }
-    }
-    
-    func logout() {
-        dismiss(animated: true) {
-            UserDefaults.standard.removeObject(forKey: "loginType")
-        }
-    }
-    
-    func kakaoLogoutBtnInit() {
-        logoutButton.rx.tap.subscribe { _ in
-            UserApi.shared.rx.logout()
-                .subscribe(onCompleted:{
-                    print("logout() success.")
-                    self.logout()
-                }, onError: {error in
-                    print(error)
-                })
-                .disposed(by: self.disposeBag)
-        } onError: { error in
-            print(error)
-        }.disposed(by: self.disposeBag)
-    }
-    
-    func naverLogoutBtnInit() {
-        logoutButton.rx.tap.subscribe { _ in
-            NaverThirdPartyLoginConnection.getSharedInstance()?.requestDeleteToken()
-            self.logout()
-        } onError: { error in
-            print(error)
-        }.disposed(by: self.disposeBag)
-    }
+//    // MARK: Actions
+//    @IBOutlet var titleLabel: UILabel!
+//    @IBOutlet var logoutButton: UIButton!
+//    
+//    // MARK: Variables
+//    var disposeBag = DisposeBag()
+//    
+//    // MARK: Methods
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        
+//        let loginType = UserDefaults.standard.value(forKey: "loginType") as! String
+//        if loginType.elementsEqual("Kakao") {
+//            kakaoLogoutBtnInit()
+//        } else if loginType.elementsEqual("Naver") {
+//            naverLogoutBtnInit()
+//        }
+//    }
+//    
+//    func logout() {
+//        dismiss(animated: true) {
+//            UserDefaults.standard.removeObject(forKey: "loginType")
+//        }
+//    }
+//    
+//    func kakaoLogoutBtnInit() {
+//        logoutButton.rx.tap.subscribe { _ in
+//            UserApi.shared.rx.logout()
+//                .subscribe(onCompleted:{
+//                    print("logout() success.")
+//                    self.logout()
+//                }, onError: {error in
+//                    print(error)
+//                })
+//                .disposed(by: self.disposeBag)
+//        } onError: { error in
+//            print(error)
+//        }.disposed(by: self.disposeBag)
+//    }
+//    
+//    func naverLogoutBtnInit() {
+//        logoutButton.rx.tap.subscribe { _ in
+//            NaverThirdPartyLoginConnection.getSharedInstance()?.requestDeleteToken()
+//            self.logout()
+//        } onError: { error in
+//            print(error)
+//        }.disposed(by: self.disposeBag)
+//    }
 }
