4967f2695227c733894b05fc3275837309253542
Byunsangjin <qustkdwls1@naver.com>
2021-01-22 00:07:59 +0900

    [TradingLog] TitleLabel 추가

--- ShareNote/ShareNote/ViewControllers/TradingLog/TradingLogMainViewController.swift
+++ ShareNote/ShareNote/ViewControllers/TradingLog/TradingLogMainViewController.swift
@@ -5,12 +5,20 @@
 //  Created by sjbyun on 2021/01/21.
 //
 
+import Then
 import UIKit
 
 class TradingLogMainViewController: UIViewController {
-
+    
+    let titleLabel = UILabel().then {
+        $0.text = "매매일지"
+        $0.font = UIFont.boldSystemFont(ofSize: 18)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setUI()
     }
     
     init() {
@@ -20,5 +28,22 @@ class TradingLogMainViewController: UIViewController {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setUI() {
+        view.backgroundColor = .white
+        
+        view.addSubview(titleLabel)
+        
+        view.setNeedsUpdateConstraints()
+    }
+    
+    override func updateViewConstraints() {
+        titleLabel.snp.makeConstraints { make in
+            make.centerX.equalTo(view)
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(15)
+        }
+        
+        super.updateViewConstraints()
     }
 }
