d4ca886be5a6ff0b5f647208367ecd1f17ef1f13
Byunsangjin <qustkdwls1@naver.com>
2021-01-28 23:07:23 +0900

    [TradingLog] 리팩토링
    - textColor, font, alignment 세팅
    - autoLayout 수정

--- ShareNote/ShareNote/ViewControllers/TradingLog/TradingNoneTableViewCell.swift
+++ ShareNote/ShareNote/ViewControllers/TradingLog/TradingNoneTableViewCell.swift
@@ -23,11 +23,17 @@ class TradingNoneTableViewCell: UITableViewCell {
     
     let titleLabel = UILabel().then {
         $0.text = "작성한 매매일지가 없어요"
+        $0.textAlignment = .center
+        $0.font = UIFont.systemFont(ofSize: 14)
+        $0.textColor = UIColor(red: 66/255, green: 66/255, blue: 66/255, alpha: 1)
     }
     
     let createButton = UIButton().then {
         $0.setTitle("작성하러가기", for: .normal)
         $0.setTitleColor(.gray, for: .normal)
+        $0.titleLabel?.textAlignment = .center
+        $0.titleLabel?.font = UIFont.systemFont(ofSize: 14)
+        $0.titleLabel?.textColor = UIColor(red: 158/255, green: 158/255, blue: 158/255, alpha: 1)
     }
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
@@ -64,19 +70,23 @@ class TradingNoneTableViewCell: UITableViewCell {
         }
         
         iconImageView.snp.makeConstraints { make in
-            make.top.equalTo(contentView).offset(14)
+            make.top.equalTo(containerView).offset(14)
             make.width.height.equalTo(30)
-            make.centerX.equalTo(contentView)
+            make.centerX.equalTo(containerView)
         }
         
         titleLabel.snp.makeConstraints { make in
             make.top.equalTo(iconImageView.snp.bottom)
-            make.centerX.equalTo(contentView)
+            make.left.right.equalTo(containerView)
+            make.height.equalTo(20)
         }
         
         createButton.snp.makeConstraints { make in
             make.top.equalTo(titleLabel.snp.bottom).offset(10)
-            make.bottom.centerX.equalTo(contentView)
+            make.left.right.equalTo(containerView)
+            make.bottom.equalTo(containerView.snp.bottom).offset(-21)
+            make.height.equalTo(18)
+            make.centerX.equalTo(containerView)
         }
         
         super.updateConstraints()
