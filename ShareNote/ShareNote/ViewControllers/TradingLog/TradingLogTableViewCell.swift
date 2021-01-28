772c04fb1ff855dd459056e8d40719f7989a87ec
Byunsangjin <qustkdwls1@naver.com>
2021-01-28 23:08:14 +0900

    [TradingLog] 매매일지 게시물 셀 추가

---
+++ ShareNote/ShareNote/ViewControllers/TradingLog/TradingLogTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TradingLogTableViewCell.swift
+//  ShareNote
+//
+//  Created by sjbyun on 2021/01/28.
+//
+
+import UIKit
+
+class TradingLogTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
