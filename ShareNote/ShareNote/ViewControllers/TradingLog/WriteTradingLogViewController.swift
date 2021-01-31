28049129d7590c1fd8d4e53e4b09a1d7b6879822
Byunsangjin <qustkdwls1@naver.com>
2021-01-31 18:58:57 +0900

    [TradingLog] 매매일지 작성 페이지 추가

---
+++ ShareNote/ShareNote/ViewControllers/TradingLog/WriteTradingLogViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WriteTradingLogViewController.swift
+//  ShareNote
+//
+//  Created by sjbyun on 2021/01/31.
+//
+
+import UIKit
+
+class WriteTradingLogViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+}
