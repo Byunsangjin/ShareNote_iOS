a8c3ca66db318f385f002f1d4cbab6c432b5201a
Byunsangjin <qustkdwls1@naver.com>
2021-02-04 23:17:49 +0900

    [TradingLog] 매매일지 작성 페이지 구현 중
    - ArticlePagerViewCell 추가

---
+++ ShareNote/ShareNote/ViewControllers/TradingLog/ArticlePagerViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ArticlePagerViewCell.swift
+//  ShareNote
+//
+//  Created by sjbyun on 2021/02/04.
+//
+
+import FSPagerView
+import UIKit
+
+class ArticlePagerViewCell: FSPagerViewCell {
+    
+}
