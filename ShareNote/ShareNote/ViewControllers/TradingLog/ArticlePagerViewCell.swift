22c59d396f4587744cf2f017d93ac0f156c616db
Byunsangjin <qustkdwls1@naver.com>
2021-02-04 23:52:26 +0900

    [TradingLog] 매매일지 작성 페이지 구현 중
    - ArticlePagerViewCell 구현

--- ShareNote/ShareNote/ViewControllers/TradingLog/ArticlePagerViewCell.swift
+++ ShareNote/ShareNote/ViewControllers/TradingLog/ArticlePagerViewCell.swift
@@ -9,8 +9,37 @@ import FSPagerView
 import UIKit
 
 class ArticlePagerViewCell: FSPagerViewCell {
+    
+    // MARK: Constants
     let containerView = UIView().createTradingShareCellView()
-        
+    
+    let thumbnailImageView = UIImageView().then {
+        $0.backgroundColor = UIColor(red: 224/255, green: 224/255, blue: 224/255, alpha: 1)
+    }
+    
+    let titleContainerView = UIView()
+    
+    let newpaperLabel = UILabel().then {
+        $0.text = "스타뉴스"
+        $0.font = UIFont.systemFont(ofSize: 10)
+        $0.textColor = UIColor(red: 97/255, green: 97/255, blue: 97/255, alpha: 1)
+        $0.numberOfLines = 0
+    }
+    
+    let headlineLabel = UILabel().then {
+        $0.text = "주식 엄청난 수익률 당설해 출연료 안받아도 된다 한 연예인"
+        $0.font = UIFont.systemFont(ofSize: 14)
+        $0.textColor = UIColor(red: 33/255, green: 33/255, blue: 33/255, alpha: 1)
+        $0.numberOfLines = 0
+    }
+    
+    let articleContentLabel = UILabel().then {
+        $0.text = "비용전가 미 38개주 구글반독점 소송가세소비자에게 비용전가 미 38개주 구글 반독점 소송가세소비자에게 비용 전가미 38개주 구글 반독점 소송가세 소송가세소게 비용 전가미 38개주 구글 반독점 소송가세 소송가세38개주 구글반독점 소송가세소비자에게 비용전가 미 38개주 구글 반독점 소송가세소비자에게 비용 전가미 38개주 구글 반독점 소송가세 소송가세소게 비용 전가미 38개주 구글 반독점 소송가세 소송가"
+        $0.font = UIFont.systemFont(ofSize: 12)
+        $0.textColor = UIColor(red: 66/255, green: 66/255, blue: 66/255, alpha: 1)
+        $0.numberOfLines = 0
+    }
+    
     let editButton = UIButton().then {
         $0.setTitle("기사 본문 보기", for: .normal)
         $0.setTitleColor(UIColor(red: 97/255, green: 97/255, blue: 97/255, alpha: 1), for: .normal)
@@ -23,6 +52,7 @@ class ArticlePagerViewCell: FSPagerViewCell {
         $0.backgroundColor = .yellow
     }
     
+    // MARK: Methods
     override init(frame: CGRect) {
         super.init(frame: frame)
         setUI()
@@ -36,7 +66,13 @@ class ArticlePagerViewCell: FSPagerViewCell {
     func setUI() {
         contentView.addSubview(containerView)
         
+        containerView.addSubview(thumbnailImageView)
         
+        containerView.addSubview(titleContainerView)
+        titleContainerView.addSubview(newpaperLabel)
+        titleContainerView.addSubview(headlineLabel)
+        
+        containerView.addSubview(articleContentLabel)
         
         containerView.addSubview(editButton)
         containerView.addSubview(deleteButton)
@@ -50,6 +86,37 @@ class ArticlePagerViewCell: FSPagerViewCell {
             make.bottom.equalTo(contentView).offset(-1.5)
             make.centerX.equalTo(contentView)
             make.width.equalTo(contentView.snp.width).offset(-40)
+        }
+        
+        thumbnailImageView.snp.makeConstraints { make in
+            make.top.equalTo(containerView).offset(15)
+            make.left.equalTo(containerView).offset(20)
+            make.width.equalTo(90)
+            make.height.equalTo(60)
+        }
+        
+        titleContainerView.snp.makeConstraints { make in
+            make.top.equalTo(thumbnailImageView)
+            make.left.equalTo(thumbnailImageView.snp.right).offset(14)
+            make.right.equalTo(containerView).offset(-26)
+            make.height.equalTo(60)
+        }
+        
+        newpaperLabel.snp.makeConstraints { make in
+            make.top.left.right.equalTo(titleContainerView)
+            make.height.equalTo(12)
+        }
+        
+        headlineLabel.snp.makeConstraints { make in
+            make.top.equalTo(newpaperLabel.snp.bottom).offset(5)
+            make.left.right.bottom.equalTo(titleContainerView)
+        }
+        
+        articleContentLabel.snp.makeConstraints { make in
+            make.top.equalTo(thumbnailImageView.snp.bottom).offset(15)
+            make.left.equalTo(containerView).offset(20)
+            make.right.equalTo(containerView).offset(-26)
+            make.bottom.equalTo(editButton.snp.top).offset(-25)
         }
         
         editButton.snp.makeConstraints { make in
