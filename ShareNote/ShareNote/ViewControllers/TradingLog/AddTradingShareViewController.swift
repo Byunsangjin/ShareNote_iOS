1bfe1aae193f34164b98006296ebe77e4d3d6ad7
Byunsangjin <qustkdwls1@naver.com>
2021-02-05 00:39:31 +0900

    [TradingLog] 매매일지 작성 페이지 구현 중
    - 거래주식 페이지 추가

---
+++ ShareNote/ShareNote/ViewControllers/TradingLog/AddTradingShareViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AddTradingShareViewController.swift
+//  ShareNote
+//
+//  Created by sjbyun on 2021/02/05.
+//
+
+import UIKit
+
+class AddTradingShareViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
