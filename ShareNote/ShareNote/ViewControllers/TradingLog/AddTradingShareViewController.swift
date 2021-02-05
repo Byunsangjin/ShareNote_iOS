cb46e914adc80d465e454a43afe3d40edc4818ee
Byunsangjin <qustkdwls1@naver.com>
2021-02-05 22:52:13 +0900

    [TradingLog] 매매일지 작성 페이지 구현 중
    - 거래주식 추가 페이지 타이틀/닫기 버튼 추가

--- ShareNote/ShareNote/ViewControllers/TradingLog/AddTradingShareViewController.swift
+++ ShareNote/ShareNote/ViewControllers/TradingLog/AddTradingShareViewController.swift
@@ -10,6 +10,30 @@ import UIKit
 class AddTradingShareViewController: UIViewController {
 
     // MARK: Constants
+    let titleLabel = UILabel().then {
+        $0.text = "거래주식"
+        $0.font = UIFont.boldSystemFont(ofSize: 16)
+        $0.textColor = UIColor(red: 33/255, green: 33/255, blue: 33/255, alpha: 1)
+    }
+    
+    let closeButton = UIButton().then {
+        $0.setImage(UIImage(named: "icClose"), for: .normal)
+        $0.setImage(UIImage(named: "icClose"), for: .highlighted)
+    }
+    
+    let scrollView = UIScrollView().then {
+        $0.showsVerticalScrollIndicator = false
+        $0.showsHorizontalScrollIndicator = false
+    }
+    
+    let scrollContentView = UIView().then {
+        $0.backgroundColor = .green
+    }
+    
+    let contentStackView = UIStackView().then {
+        $0.axis = .vertical
+        $0.translatesAutoresizingMaskIntoConstraints = false
+    }
     
     // MARK: Methods
     override func viewDidLoad() {
@@ -29,10 +53,42 @@ class AddTradingShareViewController: UIViewController {
     func setUI() {
         view.backgroundColor = .white
         
+        view.addSubview(titleLabel)
+        view.addSubview(closeButton)
+        
+        view.addSubview(scrollView)
+        scrollView.addSubview(scrollContentView)
+        scrollContentView.addSubview(contentStackView)
+        
         view.setNeedsUpdateConstraints()
     }
     
     override func updateViewConstraints() {
+        titleLabel.snp.makeConstraints { make in
+            make.centerX.equalTo(view.safeAreaLayoutGuide)
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(14)
+        }
+        
+        closeButton.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(8)
+            make.right.equalTo(view.safeAreaLayoutGuide).offset(-15)
+            make.width.height.equalTo(30)
+        }
+        
+        scrollView.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(12)
+            make.bottom.left.width.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        scrollContentView.snp.makeConstraints { make in
+            make.top.left.bottom.equalTo(scrollView)
+            make.width.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        contentStackView.snp.makeConstraints { make in
+            make.height.equalTo(250)
+        }
+        
         super.updateViewConstraints()
     }
 }
