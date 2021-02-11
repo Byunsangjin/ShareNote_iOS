5ed9fc6d4e71bdf32e55df6aefd9c8b012373880
Byunsangjin <qustkdwls1@naver.com>
2021-02-12 01:37:04 +0900

    SegmentControl관련 Extension 추가

---
+++ ShareNote/ShareNote/Extension/UIImaeg+Extension.swift
@@ -0,0 +1,21 @@
+//
+//  UIImaeg+Extension.swift
+//  ShareNote
+//
+//  Created by sjbyun on 2021/02/12.
+//
+
+import UIKit
+
+extension UIImage{
+    class func getColoredRectImageWith(color: CGColor, andSize size: CGSize) -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
+        let graphicsContext = UIGraphicsGetCurrentContext()
+        graphicsContext?.setFillColor(color)
+        let rectangle = CGRect(x: 0.0, y: 0.0, width: size.width, height: size.height)
+        graphicsContext?.fill(rectangle)
+        let rectangleImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return rectangleImage!
+    }
+}
---
+++ ShareNote/ShareNote/Extension/UISegmentControl+Extension.swift
@@ -0,0 +1,43 @@
+//
+//  UISegmentControl+Extension.swift
+//  ShareNote
+//
+//  Created by sjbyun on 2021/02/12.
+//
+
+import UIKit
+
+extension UISegmentedControl{
+    func removeBorder(){
+        let backgroundImage = UIImage.getColoredRectImageWith(color: UIColor.white.cgColor, andSize: self.bounds.size)
+        self.setBackgroundImage(backgroundImage, for: .normal, barMetrics: .default)
+        self.setBackgroundImage(backgroundImage, for: .selected, barMetrics: .default)
+        self.setBackgroundImage(backgroundImage, for: .highlighted, barMetrics: .default)
+        
+        let deviderImage = UIImage.getColoredRectImageWith(color: UIColor.white.cgColor, andSize: CGSize(width: 1.0, height: self.bounds.size.height))
+        self.setDividerImage(deviderImage, forLeftSegmentState: .selected, rightSegmentState: .normal, barMetrics: .default)
+        self.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor(red: 189/255, green: 189/255, blue: 189/255, alpha: 1)], for: .normal)
+        self.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.black], for: .selected)
+    }
+    
+    func addUnderlineForSelectedSegment(){
+        removeBorder()
+        let underlineWidth: CGFloat = self.bounds.size.width / CGFloat(self.numberOfSegments)
+        let underlineHeight: CGFloat = 2.0
+        let underlineXPosition = CGFloat(selectedSegmentIndex * Int(underlineWidth))
+        let underLineYPosition = self.bounds.size.height - 1.0
+        let underlineFrame = CGRect(x: underlineXPosition, y: underLineYPosition, width: underlineWidth, height: underlineHeight)
+        let underline = UIView(frame: underlineFrame)
+        underline.backgroundColor = UIColor(red: 255/255, green: 214/255, blue: 8/255, alpha: 1.0)
+        underline.tag = 1
+        self.addSubview(underline)
+    }
+    
+    func changeUnderlinePosition(){
+        guard let underline = self.viewWithTag(1) else {return}
+        let underlineFinalXPosition = (self.bounds.width / CGFloat(self.numberOfSegments)) * CGFloat(selectedSegmentIndex)
+        UIView.animate(withDuration: 0.3, animations: {
+            underline.frame.origin.x = underlineFinalXPosition
+        })
+    }
+}
