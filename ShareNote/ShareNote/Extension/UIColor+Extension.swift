46aefe259c5caa8ba400918dffc09147ea1a16d4
Byunsangjin <qustkdwls1@naver.com>
2021-02-13 00:32:37 +0900

    UIColor Extension 리팩토링

--- ShareNote/ShareNote/Extension/UIColor+Extension.swift
+++ ShareNote/ShareNote/Extension/UIColor+Extension.swift
@@ -8,91 +8,128 @@
 import UIKit
 
 extension UIColor {
+    /**
+     rgb 0 0 0
+     */
     class var black1: UIColor {
         guard let color = UIColor(named: "black1") else { return .black }
         return color
     }
-    
+    /**
+     rgb 33 33 33
+     */
     class var black2: UIColor {
         guard let color = UIColor(named: "black2") else { return .black }
         return color
     }
-    
+    /**
+     rgb 66 66 66
+     */
     class var grey1: UIColor {
         guard let color = UIColor(named: "grey1") else { return .black }
         return color
     }
-    
+    /**
+     rgb 97 97 97
+     */
     class var grey2: UIColor {
         guard let color = UIColor(named: "grey2") else { return .black }
         return color
     }
-    
+    /**
+     rgb 117 117 117
+     */
     class var grey3: UIColor {
         guard let color = UIColor(named: "grey3") else { return .black }
         return color
     }
-    
+    /**
+     rgb 158 158 158
+     */
     class var grey4: UIColor {
         guard let color = UIColor(named: "grey4") else { return .black }
         return color
     }
-    
+    /**
+     rgb 189 189 189
+     */
     class var grey5: UIColor {
         guard let color = UIColor(named: "grey5") else { return .black }
         return color
     }
-    
+    /**
+     rgb 224 224 224
+     */
     class var grey6: UIColor {
         guard let color = UIColor(named: "grey6") else { return .black }
         return color
     }
-    
+    /**
+     rgb 238 238 238
+     */
     class var grey7: UIColor {
         guard let color = UIColor(named: "grey7") else { return .black }
         return color
     }
-    
+    /**
+     rgb 245 245 245
+     */
     class var tempGreyColor: UIColor {
         guard let color = UIColor(named: "tempGreyColor") else { return .black }
         return color
     }
-    
+    /**
+     rgb 250 250 250
+     */
     class var grey8: UIColor {
         guard let color = UIColor(named: "grey8") else { return .black }
         return color
     }
-    
+    /**
+     rgb 255 255 255
+     */
     class var white: UIColor {
         guard let color = UIColor(named: "white") else { return .black }
         return color
     }
-    
+    /**
+     rgb 255 214 8
+     */
     class var mainColor: UIColor {
         guard let color = UIColor(named: "mainColor") else { return .black }
         return color
     }
-    
+    /**
+     rgb 255 234 132
+     */
     class var tag: UIColor {
         guard let color = UIColor(named: "tag") else { return .black }
         return color
     }
-    
+    /**
+     rgb 255 246 199
+     */
     class var tag2: UIColor {
         guard let color = UIColor(named: "tag2") else { return .black }
         return color
     }
-    
+    /**
+     rgb 111 93 0
+     */
     class var mainBrown: UIColor {
         guard let color = UIColor(named: "mainBrown") else { return .black }
         return color
     }
-    
+    /**
+     rgb 230 89 89
+     */
     class var red: UIColor {
         guard let color = UIColor(named: "red") else { return .black }
         return color
     }
-    
+    /**
+     rgb 68 182 253
+     */
     class var blue: UIColor {
         guard let color = UIColor(named: "blue") else { return .black }
         return color
