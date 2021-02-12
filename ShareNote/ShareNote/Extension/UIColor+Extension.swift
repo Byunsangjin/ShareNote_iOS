9a32909e8f2399af21bce8f63b5bdc1fcf3a8528
Byunsangjin <qustkdwls1@naver.com>
2021-02-12 18:27:55 +0900

    UIColor관련 Extension 추가

---
+++ ShareNote/ShareNote/Extension/UIColor+Extension.swift
@@ -0,0 +1,78 @@
+//
+//  UIColor+Extension.swift
+//  ShareNote
+//
+//  Created by sjbyun on 2021/02/12.
+//
+
+import UIKit
+
+extension UIColor {
+    class var black1: UIColor {
+        return UIColor(white: 0.0, alpha: 1.0)
+    }
+    
+    class var black2: UIColor {
+        return UIColor(white: 33.0 / 255.0, alpha: 1.0)
+    }
+    
+    class var grey1: UIColor {
+        return UIColor(white: 66.0 / 255.0, alpha: 1.0)
+    }
+    
+    class var grey2: UIColor {
+        return UIColor(white: 97.0 / 255.0, alpha: 1.0)
+    }
+    
+    class var grey3: UIColor {
+        return UIColor(white: 117.0 / 255.0, alpha: 1.0)
+    }
+    
+    class var grey4: UIColor {
+        return UIColor(white: 158.0 / 255.0, alpha: 1.0)
+    }
+    
+    class var grey5: UIColor {
+        return UIColor(white: 189.0 / 255.0, alpha: 1.0)
+    }
+    
+    class var grey6: UIColor {
+        return UIColor(white: 224.0 / 255.0, alpha: 1.0)
+    }
+    
+    class var grey7: UIColor {
+        return UIColor(white: 238.0 / 255.0, alpha: 1.0)
+    }
+    
+    class var grey8: UIColor {
+        return UIColor(white: 250.0 / 255.0, alpha: 1.0)
+    }
+    
+    class var white: UIColor {
+        return UIColor(white: 1.0, alpha: 1.0)
+    }
+    
+    class var maincolor: UIColor {
+        return UIColor(red: 1.0, green: 214.0 / 255.0, blue: 8.0 / 255.0, alpha: 1.0)
+    }
+    
+    class var tag: UIColor {
+        return UIColor(red: 1.0, green: 234.0 / 255.0, blue: 132.0 / 255.0, alpha: 1.0)
+    }
+    
+    class var tag2: UIColor {
+        return UIColor(red: 1.0, green: 246.0 / 255.0, blue: 199.0 / 255.0, alpha: 1.0)
+    }
+    
+    class var mainBrown: UIColor {
+        return UIColor(red: 111.0 / 255.0, green: 93.0 / 255.0, blue: 0.0, alpha: 1.0)
+    }
+    
+    class var red: UIColor {
+        return UIColor(red: 230.0 / 255.0, green: 89.0 / 255.0, blue: 89.0 / 255.0, alpha: 1.0)
+    }
+    
+    class var blue: UIColor {
+        return UIColor(red: 68.0 / 255.0, green: 182.0 / 255.0, blue: 253.0 / 255.0, alpha: 1.0)
+    }
+}
