8d345a89684ccca16da807e6cdd3fff4b224e838
Byunsangjin <qustkdwls1@naver.com>
2021-02-17 22:52:50 +0900

    [Register] 본인인증 페이지
    - 인증번호받기 페이지 추가

---
+++ ShareNote/ShareNote/ViewControllers/Register/CertificationNumberViewController.swift
@@ -0,0 +1,15 @@
+//
+//  CertificationNumberViewController.swift
+//  ShareNote
+//
+//  Created by sjbyun on 2021/02/17.
+//
+
+import UIKit
+
+class CertificationNumberViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
