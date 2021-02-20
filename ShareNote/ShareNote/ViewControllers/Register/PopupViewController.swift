966af9c8d72ca9d5a74a22eef67afbe4f8abafb0
Byunsangjin <qustkdwls1@naver.com>
2021-02-21 00:28:33 +0900

    [Register] 본인인증 페이지
    - PopupViewController 추가

---
+++ ShareNote/ShareNote/ViewControllers/Register/PopupViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PopupViewController.swift
+//  ShareNote
+//
+//  Created by sjbyun on 2021/02/21.
+//
+
+import UIKit
+
+class PopupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
