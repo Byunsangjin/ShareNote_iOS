9ec562b9afd10a6908f4cbd26a6a98630f4a8e0f
Byunsangjin <qustkdwls1@naver.com>
2021-03-14 11:23:15 +0900

    NavigationView
    - right barButton 추가

--- ShareNote/ShareNote/Custom/NavigationView.swift
+++ ShareNote/ShareNote/Custom/NavigationView.swift
@@ -14,9 +14,11 @@ class NavigationView: UIView {
         $0.font = UIFont.spoqaHanSans(size: 16, style: .Bold)
     }
     
-    let backButton = UIButton().then {
+    let leftBarButton = UIButton().then {
         $0.setImage(UIImage(named: "iconsGotobac"), for: .normal)
     }
+    
+    let rightBarButton = UIButton()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -35,7 +37,8 @@ class NavigationView: UIView {
         backgroundColor = UIColor.white
         
         self.addSubview(titleLabel)
-        self.addSubview(backButton)
+        self.addSubview(leftBarButton)
+        self.addSubview(rightBarButton)
     }
     
     override func updateConstraints() {
@@ -43,8 +46,14 @@ class NavigationView: UIView {
             make.centerX.centerY.equalTo(self)
         }
         
-        backButton.snp.makeConstraints { make in
+        leftBarButton.snp.makeConstraints { make in
             make.left.equalTo(self).offset(15)
+            make.centerY.equalTo(self)
+            make.width.height.equalTo(30)
+        }
+        
+        rightBarButton.snp.makeConstraints { make in
+            make.right.equalTo(self).offset(-15)
             make.centerY.equalTo(self)
             make.width.height.equalTo(30)
         }
