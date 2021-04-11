0c7247eecbe969a485355acc8ab94046acf43c29
Byunsangjin <qustkdwls1@naver.com>
2021-04-11 19:17:38 +0900

    애플 로그인 버튼 구현

--- ShareNote/ShareNote/ViewControllers/Login/LoginViewController.swift
+++ ShareNote/ShareNote/ViewControllers/Login/LoginViewController.swift
@@ -213,6 +213,21 @@ class LoginViewController: UIViewController {
             .bind { _ in
                 GIDSignIn.sharedInstance()?.signIn()
             }.disposed(by: disposeBag)
+        
+        
+        if #available(iOS 13.0, *) {
+            appleLoginButton.rx.tap
+                .bind { [weak self] in
+                    let appleIDProvider = ASAuthorizationAppleIDProvider()
+                    let request = appleIDProvider.createRequest()
+                    request.requestedScopes = [.fullName, .email]
+                    
+                    let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+                    authorizationController.delegate = self
+                    authorizationController.presentationContextProvider = self
+                    authorizationController.performRequests()
+                }.disposed(by: disposeBag)
+        }
     }
     
     func setUI() {
@@ -441,5 +456,38 @@ extension LoginViewController: GIDSignInDelegate {
         logger.verbose("didDisconnectWith")
         // Perform any operations when the user disconnects from app here.
         // ...
+    }
+}
+
+@available(iOS 13.0, *)
+extension LoginViewController: ASAuthorizationControllerPresentationContextProviding, ASAuthorizationControllerDelegate {
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return self.view.window!
+    }
+
+    // Apple ID 연동 성공 시
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        switch authorization.credential {
+        // Apple ID
+        case let appleIDCredential as ASAuthorizationAppleIDCredential:
+
+            // 계정 정보 가져오기
+            let userIdentifier = appleIDCredential.user
+            let fullName = appleIDCredential.fullName
+            let email = appleIDCredential.email
+
+            print("User ID : \(userIdentifier)")
+            print("User Email : \(email ?? "")")
+            print("User Name : \((fullName?.givenName ?? "") + (fullName?.familyName ?? ""))")
+            
+            presentMainTabViewController()
+        default:
+            break
+        }
+    }
+
+    // Apple ID 연동 실패 시
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        // Handle error.
     }
 }
