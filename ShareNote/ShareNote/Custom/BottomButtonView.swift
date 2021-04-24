f98e4009824b15fcf13e1ae73154ff71c581991a
Byunsangjin <qustkdwls1@naver.com>
2021-04-24 17:15:36 +0900

    BottomButton CustomView 구현

---
+++ ShareNote/ShareNote/Custom/BottomButtonView.swift
@@ -0,0 +1,20 @@
+//
+//  BottomButtonView.swift
+//  ShareNote
+//
+//  Created by sjbyun on 2021/04/24.
+//
+
+import UIKit
+
+class BottomButtonView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
