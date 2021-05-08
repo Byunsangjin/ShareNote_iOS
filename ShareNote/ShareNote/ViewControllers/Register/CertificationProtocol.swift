4d763a1bc005d386cb32f683447e5059a9c11f98
Byunsangjin <qustkdwls1@naver.com>
2021-05-08 17:11:02 +0900

    인증번호 받는 화면 Protocol 추가

---
+++ ShareNote/ShareNote/ViewControllers/Register/CertificationProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  AuthProtocol.swift
+//  ShareNote
+//
+//  Created by sjbyun on 2021/05/08.
+//
+
+import Foundation
+
+protocol CertificationProtocol {
+    func didDissmissAuthViewController(type: String?)
+}
