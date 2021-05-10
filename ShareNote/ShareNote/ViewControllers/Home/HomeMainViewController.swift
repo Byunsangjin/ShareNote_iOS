4bb3ce9f4b450674b532904af2a0368714e78aab
Byunsangjin <qustkdwls1@naver.com>
2021-05-10 21:42:48 +0900

    메인 페이지 화면 일부 구현

--- ShareNote/ShareNote/ViewControllers/Home/HomeMainViewController.swift
+++ ShareNote/ShareNote/ViewControllers/Home/HomeMainViewController.swift
@@ -25,6 +25,44 @@ class HomeMainViewController: UIViewController {
         $0.setImage(UIImage(named: "icAlarm"), for: .normal)
     }
     
+    let summaryContainerView = UIView()
+    
+    let summaryBackgroundView = UIView().then {
+        $0.backgroundColor = .mainColor.withAlphaComponent(0.1)
+    }
+    
+    let illustImageView = UIImageView().then {
+        $0.image = UIImage(named: "icMainIllustration")
+    }
+    
+    let summaryLabel = UILabel().then {
+        $0.text = "김쉐어님,\n첫 매매일지를 작성해보세요"
+        $0.textColor = .black2
+        $0.font = UIFont.spoqaHanSans(size: 14, style: .Bold)
+        $0.numberOfLines = 0
+    }
+    
+    let writeButton = UIButton().then {
+        $0.setTitle("작성하러 가기", for: .normal)
+        $0.setTitleColor(.black2, for: .normal)
+        $0.titleLabel?.font = UIFont.spoqaHanSans(size: 12)
+        $0.backgroundColor = .mainColor
+        $0.layer.cornerRadius = 5
+    }
+    
+    let shareNotiPickTitleLabel = UILabel().then {
+        $0.text = "쉐어노티 PICK"
+        $0.textColor = .black2
+        $0.font = UIFont.spoqaHanSans(size: 18, style: .Bold)
+    }
+    
+    let shareNotiPickDescriptionLabel = UILabel().then {
+        $0.text = "쉐어노트 이용자들의 핫 토픽들을 모았어요. 매일 업데이트되는\n주요 토픽들을 살펴보세요 !"
+        $0.textColor = .black2
+        $0.font = UIFont.spoqaHanSans(size: 12)
+        $0.numberOfLines = 0
+    }
+    
     // MARK: Methods
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -50,6 +88,15 @@ class HomeMainViewController: UIViewController {
         scrollContentView.addSubview(searchButton)
         scrollContentView.addSubview(alarmButton)
         
+        scrollContentView.addSubview(summaryContainerView)
+        summaryContainerView.addSubview(summaryBackgroundView)
+        summaryBackgroundView.addSubview(illustImageView)
+        summaryBackgroundView.addSubview(summaryLabel)
+        summaryBackgroundView.addSubview(writeButton)
+        
+        scrollContentView.addSubview(shareNotiPickTitleLabel)
+        scrollContentView.addSubview(shareNotiPickDescriptionLabel)
+        
         view.setNeedsUpdateConstraints()
     }
     
@@ -73,6 +120,49 @@ class HomeMainViewController: UIViewController {
             make.top.equalTo(searchButton)
             make.right.equalTo(scrollContentView).offset(-15)
             make.width.height.equalTo(30)
+        }
+        
+        summaryContainerView.snp.makeConstraints { make in
+            make.top.equalTo(searchButton.snp.bottom).offset(20)
+            make.left.right.equalTo(scrollContentView)
+            make.height.equalTo(120)
+        }
+        
+        summaryBackgroundView.snp.makeConstraints { make in
+            make.top.equalTo(summaryContainerView).offset(12)
+            make.left.equalTo(summaryContainerView).offset(18)
+            make.right.equalTo(summaryContainerView).offset(-21)
+            make.bottom.equalTo(summaryContainerView)
+        }
+        
+        illustImageView.snp.makeConstraints { make in
+            make.right.equalTo(summaryBackgroundView)
+            make.bottom.equalTo(summaryBackgroundView).offset(-12)
+            make.width.equalTo(174)
+            make.height.equalTo(100)
+        }
+        
+        summaryLabel.snp.makeConstraints { make in
+            make.top.equalTo(summaryBackgroundView).offset(14)
+            make.left.equalTo(summaryBackgroundView).offset(21)
+        }
+        
+        writeButton.snp.makeConstraints { make in
+            make.top.equalTo(summaryLabel.snp.bottom).offset(7)
+            make.left.equalTo(summaryLabel)
+            make.width.equalTo(89)
+            make.height.equalTo(30)
+        }
+        
+        shareNotiPickTitleLabel.snp.makeConstraints { make in
+            make.top.equalTo(summaryContainerView.snp.bottom).offset(39)
+            make.left.equalTo(scrollContentView).offset(20)
+            make.right.equalTo(scrollContentView).offset(-20)
+        }
+        
+        shareNotiPickDescriptionLabel.snp.makeConstraints { make in
+            make.top.equalTo(shareNotiPickTitleLabel.snp.bottom).offset(8)
+            make.left.right.equalTo(shareNotiPickTitleLabel)
         }
         
         super.updateViewConstraints()
