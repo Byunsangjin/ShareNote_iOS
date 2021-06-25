b753e35f6bf7a46e9663a0ed428e7ac8adb2f40b
Byunsangjin <qustkdwls1@naver.com>
2021-06-25 14:59:13 +0900

    검색 화면 추가

---
+++ ShareNote/ShareNote/ViewControllers/Home/SearchViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SearchViewController.swift
+//  ShareNote
+//
+//  Created by sjbyun on 2021/06/25.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
